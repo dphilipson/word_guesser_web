c69c29dc19a3b64196cfd426f4a45e8a54ed6b6f
David Philipson <david.philipson@gmail.com>
2013-12-31 18:44:56 -0800

    Add basic post skeleton

--- Handler/Game.hs
+++ Handler/Game.hs
@@ -3,9 +3,12 @@ module Handler.Game where
 import Import
 import Text.Shakespeare.Text
 
-data GameState = GameState { game :: Game
-                           , latestGuess :: Maybe Guess
+data GameState = GameState { stateGameEntity :: Entity Game
+                           , stateLatestGuess :: Maybe Guess
                            }
+
+stateSecret :: GameState -> Text
+stateSecret (GameState {stateGameEntity = Entity _ (Game {gameSecret = secret})}) = secret
 
 getGameR :: Handler Html
 getGameR = do
@@ -17,10 +20,10 @@ getGameR = do
 loadGameState :: YesodDB App GameState
 loadGameState = do
     -- Assume just one game for now.
-    Entity gid game_ <- loadGameEntity
+    gEntity@(Entity gid _) <- loadGameEntity
     eLastGuess <- selectFirst [GuessGame ==. gid] [Desc GuessCount, LimitTo 1]
-    let lastGuess_ = entityVal <$> eLastGuess
-    return $ GameState game_ lastGuess_
+    let lastGuess = entityVal <$> eLastGuess
+    return $ GameState gEntity lastGuess
     
 loadGameEntity :: YesodDB App (Entity Game)
 loadGameEntity = do
@@ -34,16 +37,33 @@ loadGameEntity = do
             return $ Entity newGid newGame 
 
 gameStateMessage :: GameState -> Text
-gameStateMessage (GameState (Game secret) (Just (Guess _ word num))) =
+gameStateMessage (GameState _ Nothing) = "Make a guess!"
+gameStateMessage gameState@(GameState _ (Just (Guess _ word num))) =
     case compare secret word of
         LT -> [st|My word comes before "#{word}" in the dictionary.|]
         GT -> [st|My word comes after "#{word}" in the dictionary.|]
         EQ -> [st|You got it! The word was "#{secret}.
                   You used #{show num} guesses."|]
-gameStateMessage (GameState _ Nothing) = "Make a guess!"
+      where secret = stateSecret gameState
 
 guessForm :: Form Text
 guessForm = renderDivs $ areq textField "Guess" Nothing
 
 postGameR :: Handler Html
-postGameR = error "Not yet implemented: postGameR"
+postGameR = do
+    ((result, _), _) <- runFormPost guessForm
+    case result of
+        FormSuccess word -> runDB $ updateWithGuess word
+        _ -> return ()
+    redirect GameR
+
+updateWithGuess :: Text -> YesodDB App ()
+updateWithGuess word = do
+    gameState <- loadGameState
+    let c = nextCount gameState
+        Entity gameId _ = stateGameEntity gameState
+    insert_ $ Guess gameId word c
+
+nextCount :: GameState -> Int
+nextCount GameState {stateLatestGuess = Nothing} = 1
+nextCount GameState {stateLatestGuess = Just (Guess _ _ c)} = c + 1
